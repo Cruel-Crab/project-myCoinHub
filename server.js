9a532122d6f577e488e63b08b1d7268c0ba99796
mohammed28 <mohammed@easyecom.io>
2022-08-01 10:10:26 +0530

    trying more fixes for deployment

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/supermind-assignment-app'));
-app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname+
-    '/dist/supermind-assignment-app/index.html'));});
+// app.use(express.static(__dirname + '/dist/supermind-assignment-app'));
+app.use(express.static('./dist/supermind-assignment-app'));
+
+// app.get('/*', function(req,res) {
+//   res.sendFile(path.join(__dirname+
+//     '/dist/supermind-assignment-app/index.html'));});
+app.get('/*', (req, res) =>
+  res.sendFile('index.html', {root: 'dist/supermind-assignment-app/'}),
+);
 app.listen(process.env.PORT || 8080);
